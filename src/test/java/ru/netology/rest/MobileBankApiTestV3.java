c8b585d0c6adce4c3014b4c57daf0b1a1a44c4d0
venomek <venom4ek@gmail.com>
2020-06-07 06:20:30 +0300

    edited test

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,8 +24,8 @@ class MobileBankApiTestV3 {
                 // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUR"))
-                .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("[1].currency", equalTo("USD"))
+                .body("[1].balance", greaterThanOrEqualTo(0))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
     }
